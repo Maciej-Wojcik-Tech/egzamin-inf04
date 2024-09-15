46deedcfc49bba31c167944a3c663d2cf3e50dfa
Maciej-Wojcik-Tech <136369949+Maciej-Wojcik-Tech@users.noreply.github.com>
2024-09-15 21:15:23 +0200

    domek w górach
    dodany

---
+++ domekMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace domekMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ domekMaui/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace domekMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+private void like_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            ilosc.Text = $"ilość polubień {count}";
+
+            SemanticScreenReader.Announce(ilosc.Text);
+        }
+
+        private void dislike_Clicked(object sender, EventArgs e)
+        {
+            if(count>0)
+            {
+
+                count--;
+
+                SemanticScreenReader.Announce(ilosc.Text);
+                ilosc.Text = $"ilość polubień {count}";
+            }
+        }
+
+        private void save_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
