38241721207ba2075b92c75eae6c4c80db77629a
Maciej-Wojcik-Tech <136369949+Maciej-Wojcik-Tech@users.noreply.github.com>
2024-11-15 19:30:40 +0100

    webowe dodane

---
+++ filmyAspNet/Data/filmyAspNetContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using filmyAspNet.Models;
+
+namespace filmyAspNet.Data
+{
+    public class filmyAspNetContext : DbContext
+    {
+        public filmyAspNetContext (DbContextOptions<filmyAspNetContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<filmyAspNet.Models.Movie> Movie { get; set; } = default!;
+    }
+}
