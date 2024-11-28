fe1e973f325a45b96caec47a9777f677a7e38d6e
Maciej-Wojcik-Tech <136369949+Maciej-Wojcik-Tech@users.noreply.github.com>
2024-11-28 08:51:02 +0100

    Create wszystkieAlgorytmy.cpp

---
+++ wszystkieAlgorytmy.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+//klasa do algorytmów wyszukiwania, sortowania, szyfrowania i numerycznych
+class Klasa {
+	protected:
+		string jawny, zaszyfrowany;
+		double lewy, prawy, wynik;
+		int tablica[]={0};
+		const double eps=0,01;
+	public:
+		int wyszukaj_liniowe(int szukana)
+		{
+			
+		}
+		int wyszukaj_wartownik(int szukana)
+		{
+			
+		}
+		int wyszukaj_bin(int szukana)
+		{
+			
+		}
+		void sort_bable()
+		{
+		}
+		void sort_wybor()
+		{
+		}
+		void sort_wstawianie()
+		{
+		}
+		void quice_sort()
+		{
+			
+		}
+		void sot_scal()
+		{
+		}
+		void zliczanie()
+		{
+		}
+		string cezar(int klucz)
+		{
+		}
+		bool palindrom(string tekst)
+		{
+		}
+		bool anagram(string tekstt1, string tekst2)
+		{
+			
+		}
+		double pole(double lewy, double prawy, int n)
+		{
+			
+		}
+		double pierwiastek(double liczba, double stopień)
+		{
+			
+		}
+};
